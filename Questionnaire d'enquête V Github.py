758c666f3267546ec4ff0815a455937daeb512b2
gasparchapatte <150336730+gasparchapatte@users.noreply.github.com>
2024-05-01 12:56:15 +0200

    Update Questionnaire d'enquête V Github.py

--- Questionnaire d'enquête V Github.py
+++ Questionnaire d'enquête V Github.py
@@ -258,6 +258,7 @@ plt.scatter(X_pca[indices_1, 0], X_pca[indices_1, 1], color='r', label='Classe 1
 plt.xlabel('First principal component')
 plt.ylabel('Second principal component')
 plt.title('Data visualisation after ACP')
+plt.legend()
 
 # K-means classification
 kmeans = KMeans(n_clusters=6, random_state=42)
@@ -302,6 +303,7 @@ plt.scatter(X_pca[indices_4, 0], X_pca[indices_4, 1], color='c', label='Classe 4
 plt.xlabel('First principal component')
 plt.ylabel('Second principal component')
 plt.title('Data visualisation after ACP')
+plt.legend()
 
 # Principal Components Analysis colored according to k-means groups 
 kmeans = KMeans(n_clusters=6, random_state=42)
@@ -333,3 +335,4 @@ plt.scatter(X_pca[indices_5, 0], X_pca[indices_5, 1], color='b', label='Classe 5
 plt.xlabel('First principal component')
 plt.ylabel('Second principal component')
 plt.title('Data visualisation after ACP')
+plt.legend()
